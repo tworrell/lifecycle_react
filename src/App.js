2cf4e13f7410a7adf993a5d4c100232e726b12d2
tworrell <tara.worrell@gmail.com>
2020-03-03 20:40:09 -0500

    linked the API

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      items: [],
+      isLoaded: false,
+    };
+  };
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then(res => {
+      return res.json();
+    })
+    .then(data => {
+      console.log(data);
+      this.setState({
+        items: data,
+        isLoaded: true,
+      });
+    });
+  };
+
+  render() {
+    var {isLoaded, items } = this.state;
+    // var { isLoaded, items } = this.state;
+    if(!isLoaded) {
+      return <div>Loading.....</div>;
+    }else {
+    return(
+      <div>
+        <ul>
+          {items.map(el => {
+            return(
+              <li key={el.id}>
+              Name: {el.name}| UserName: {el.username} | {' '}
+              <a href ={`https:// ${el.website}`}>Website</a>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+    );
+  };
+  };
+};
 
 export default App;
